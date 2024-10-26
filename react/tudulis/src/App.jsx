b6e628875182ee0ceb6e0f0ca0229c99324017f3
Mashu <federicomatthewpratamaa@gmail.com>
2024-10-26 19:32:55 +0700

    Update App.jsx

--- react/tudulis/src/App.jsx
+++ react/tudulis/src/App.jsx
@@ -1,8 +1,8 @@
 function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <div className="flex">
+      <div>Hello World!</div>
+    </div>
   );
 }
 
